8c713b4fc77afe1d2b7efd52f8fbea7c35bb1fa0
陈通涨 <2982179645@qq.com>
2024-09-18 09:16:09 +0800

    第一次上传

---
+++ src/main/java/org/example/HmDianPingApplication.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@MapperScan("org.example.mapper")
+@SpringBootApplication
+public class HmDianPingApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HmDianPingApplication.class, args);
+    }
+}
